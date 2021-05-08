1ebfa60e614ae75e1ed87fdc60d34a2b9245089c
María Fernanda Ramírez <mframirezfuentes@gmail.com>
2021-05-08 13:04:57 -0400

    cambios en los id de las secciones, en sus margenes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,14 @@ $(document).ready(function () {
     
 
     $('[data-toggle="popover"]').popover()
+    $("a").click(function (evento) { 
+        evento.preventDefault();
+        var gato= this.hash;
+        $("html").animate({
+            scrollTop: $(gato).offset().top,
+        }, 800)
+        
+    });
 });
 
    
